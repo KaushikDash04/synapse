7d9ff464063106e36a3003c318ab646e4e90864f
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-02-10 21:55:40 -0800

    fronted added

---
+++ FRONTEND/src/App.js
@@ -0,0 +1,278 @@
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+import "./App.css"; // Import the CSS file for styling
+import Sidebar from "./components/Sidebar";
+import SendSharpIcon from "@mui/icons-material/SendSharp";
+import KeyboardDoubleArrowUpSharpIcon from "@mui/icons-material/KeyboardDoubleArrowUpSharp";
+import PsychologyRoundedIcon from "@mui/icons-material/PsychologyRounded";
+import PsychologyAltRoundedIcon from "@mui/icons-material/PsychologyAltRounded";
+import TypewriterAnimation from "./TypewriterAnimation"; // Import the TypewriterAnimation component
+
+function App() {
+  const [question, setQuestion] = useState("");
+  const [model, setModel] = useState("gemini-pro");
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState("");
+  const [image, setImage] = useState(null);
+  const [pdf, setPdf] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null); // State to store image preview
+  const [chatHistory, setChatHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); // Loading state
+  const chatBottomRef = useRef(null); // Reference to the bottom of the chat area
+
+  useEffect(() => {
+    // Scroll to the bottom of the chat area when chatHistory changes
+    chatBottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [chatHistory]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setQuestion("");
+    try {
+      // Add the user's question to chat history
+      setChatHistory((prevChat) => [
+        ...prevChat,
+        { question: question, response: "", loading: true }, // Include loading state in chat history
+      ]);
+
+      // Set loading state to true while waiting for response
+      setIsLoading(true);
+
+      // Make API call to get response
+      const generateResponse = await axios.get(
+        `http://127.0.0.1:8000/generate_text_gemini/?model_name=${model}&question=${question}`
+      );
+      const newResponse = generateResponse.data["AI Response"];
+
+      // Update chat history with the response
+      setChatHistory((prevChat) => {
+        const updatedChat = [...prevChat];
+        const lastChatIndex = updatedChat.length - 1;
+        updatedChat[lastChatIndex].response = newResponse;
+        updatedChat[lastChatIndex].loading = false; // Set loading state to false after response is received
+        return updatedChat;
+      });
+
+      // Set response and error states
+      setResponse(newResponse);
+      setError("");
+
+      // Set loading state to false after response is received
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error:", error);
+      if (error.response) {
+        setError(error.response.data.detail);
+      } else {
+        setError(
+          "An error occurred while processing your request. Please try again later."
+        );
+      }
+
+      // Set loading state to false if there's an error
+      setIsLoading(false);
+    }
+  };
+
+  const handleImageChange = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  const handlePdfChange = (e) => {
+    setPdf(e.target.files[0]);
+  };
+
+  return (
+    <>
+      <div
+        className="app-container"
+        style={{ display: "flex", opacity: "50%" }}
+      >
+        {/* Sidebar component */}
+        <Sidebar imagePreview={imagePreview} />
+
+        {/* Chatbot container */}
+        <div className="chatbot-container">
+          <div className="chatbot-header"></div>
+          <div
+            className="chatbot-chat-area"
+            style={{
+              backgroundColor: "#171717",
+              color: "#a7bfe8",
+              marginLeft: "320px",
+              padding: "50px",
+              maxHeight: "550px",
+              overflowY: "auto",
+              width: "1092px",
+            }}
+          >
+            {chatHistory.map((chat, index) => (
+              <div key={index} className="chatbot-message">
+                <p
+                  style={{
+                    textAlign: "right",
+                    padding: "10px",
+                    color: "violet",
+                  }}
+                >
+                  <PsychologyAltRoundedIcon sx={{ fontSize: 40, mt: "20px" }} />
+                  <strong>USER:</strong> <br />
+                  {chat.question}
+                </p>
+                <p className="synapse-message">
+                  <PsychologyRoundedIcon sx={{ fontSize: 40 }} />
+                  <strong>SYNAPSE:</strong> <br />
+                  {chat.loading ? (
+                    <TypewriterAnimation text="Thinking..." />
+                  ) : (
+                    <TypewriterAnimation text={chat.response} />
+                  )}
+                </p>
+              </div>
+            ))}
+
+            {/* Empty div to keep track of the bottom of the chat area */}
+            <div ref={chatBottomRef}></div>
+          </div>
+
+          {/* Chat input form */}
+          <form
+            onSubmit={handleSubmit}
+            className="chatbot-input-form"
+            style={{
+              backgroundColor: "transparent",
+              position: "fixed",
+              top: "50px",
+              left: "430px",
+              width: "2000px",
+              gap: "5px",
+            }}
+          >
+            {/* Upload section */}
+            <div className="upload-section">
+              {(model === "gemini-vision" || model === "pdf-gpt") && (
+                <>
+                  <label
+                    htmlFor={model === "gemini-vision" ? "image" : "pdf"}
+                    className="upload-button"
+                    style={{ backgroundColor: "#2d2d2d", borderRadius: "12px" }}
+                  >
+                    <UploadFileIcon sx={{ mt: "10px", bgcolor: "#2d2d2d" }} />
+                  </label>
+                  <input
+                    type="file"
+                    id={model === "gemini-vision" ? "image" : "pdf"}
+                    accept={model === "gemini-vision" ? "image/*" : ".pdf"}
+                    onChange={
+                      model === "gemini-vision"
+                        ? handleImageChange
+                        : handlePdfChange
+                    }
+                    required
+                    className="file-input"
+                  />
+                </>
+              )}
+            </div>
+
+            {/* Model select form */}
+            <div className="model-select-form">
+              <label
+                htmlFor="model-select"
+                className="model-select-label"
+              ></label>
+              <select
+                id="model-select"
+                value={model}
+                onChange={(e) => setModel(e.target.value)}
+                className="model-select"
+                style={{
+                  marginTop: "20px",
+                  backgroundColor: "#2d2d2d",
+                  border: "none",
+                  borderRadius: "12px",
+                  width: "150px",
+                  color: "white",
+                }}
+              >
+                <option value="gemini-pro">Gemini Pro</option>
+                <option value="gemini-vision">Gemini Vision</option>
+                <option value="pdf-gpt">PDF GPT</option>
+              </select>
+            </div>
+
+            {/* Input field */}
+            <input
+              type="text"
+              value={question}
+              className="input"
+              sx={{
+                "& input": {
+                  color: "white",
+                },
+                "& .MuiInput-underline:before": {
+                  borderBottom: "none",
+                },
+                "& .MuiInput-underline:after": {
+                  borderBottom: "none",
+                },
+              }}
+              style={{
+                width: "700px",
+                backgroundColor: "#2d2d2d",
+                paddingLeft: "30px",
+                height: "40px",
+                marginTop: "0px",
+                borderRadius: "12px",
+                color: "white",
+                border: "0px",
+              }}
+              onChange={(e) => setQuestion(e.target.value)}
+              required
+              placeholder="Enter your question..."
+            />
+
+            {/* Submit button */}
+            <Button
+              className="send"
+              type="submit"
+              variant="contained"
+              color="primary"
+              style={{
+                left: "-80px",
+                backgroundColor: "transparent",
+                border: "none",
+                width: "1px",
+                boxShadow: "none",
+              }}
+            >
+              <KeyboardDoubleArrowUpSharpIcon />
+            </Button>
+          </form>
+        </div>
+      </div>
+      <h5
+        className="foot"
+        style={{
+          position: "fixed",
+          color: "gray",
+          marginLeft: "740px",
+          marginTop: "635px",
+        }}
+      >
+        Synapse can make mistakes. Consider checking important information.
+      </h5>
+    </>
+  );
+}
+
+export default App;
---
+++ FRONTEND/src/components/Sidebar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react"; // Import useState
+import "./Sidebar.css"; // Import the CSS file for styling
+import AutoModeRoundedIcon from '@mui/icons-material/AutoModeRounded';
+import AddCircleOutlineSharpIcon from '@mui/icons-material/AddCircleOutlineSharp';
+
+// const [imagePreview, setImagePreview] = useState('');
+// const [pdfPreview, setPdfPreview] = useState('');
+
+function Sidebar({ imagePreview, pdfPreview }) { // Receive both imagePreview and pdfPreview props
+
+  function refreshPage() {
+    window.location.reload();
+}
+
+  return (
+    <div className="sidebar">
+      {/* Sidebar content */}
+      <div className="sidebar-content">
+        <h1>SYNAPSE     <AutoModeRoundedIcon/></h1>
+         {/* Add any additional sidebar content here */}
+         <button style={{width:"300px", borderRadius:"12px",display:"flex",marginTop:"20px"}}
+         onClick={refreshPage}>
+          <div className="new" style={{display:"flex", gap:"5px"}}><div style={{marginTop:"13px"}}><AddCircleOutlineSharpIcon/></div><h3>New Chat</h3></div></button>
+        {imagePreview && (
+          <div className="image">
+            <h2>Uploaded Image:</h2>
+            <img src={imagePreview} alt="Uploaded" style={{ maxWidth: '100%', maxHeight: '200px' }} />
+          </div>
+        )}
+        {pdfPreview && (
+          <div>
+            <h2>Uploaded PDF:</h2>
+            <embed src={pdfPreview} type="application/pdf" width="50%" height="400px" />
+          </div>
+        )}
+      </div>
+      <h3 className="developer">Designed and devloped by<br/>Kaushik and Hardeep.</h3>
+    </div>
+  );
+}
+
+export default Sidebar;
