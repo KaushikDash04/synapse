eb65a39bb9d5903edc0a6cf5c97e525d96064539
Kaushik Dash <kaushik.dash8@gmail.com>
2024-06-23 13:56:29 +0530

    added markdown

--- FRONTEND/src/TypewriterAnimation.jsx
+++ FRONTEND/src/TypewriterAnimation.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ReactMarkdown from 'react-markdown';
 
 const TypewriterAnimation = ({ text }) => {
   const [displayText, setDisplayText] = useState("");
@@ -19,7 +20,7 @@ const TypewriterAnimation = ({ text }) => {
     };
   }, [currentIndex, text]);
 
-  return <span>{displayText}</span>;
+  return <span><ReactMarkdown>{displayText}</ReactMarkdown></span>;
 };
 
 export default TypewriterAnimation;
